0da295accb0aaf84777c90134425f824719bd6dd
Marc Sànchez Pifarré <msanxes@gmail.com>
2019-06-20 12:28:38 +0200

    [WIP] Auctioner

--- src/Auctioner.java
+++ src/Auctioner.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 /**
  * Subhastador.
@@ -17,9 +18,9 @@ public class Auctioner {
         // Subhastem tots els focs a tots els robots.
         for (Foc f : escenari.Focs) {
             Voyage winner = null;
-            List<Voyage> offers = new ArrayList<>();
+            TreeSet<Voyage> offers = new TreeSet<>();
             for (Robot r : escenari.Robots) {
-
+                offers.add(r.getOffer(f));
             }
             // Afegeixo el viatge al robot guanyador.
             winner.getRobot().update(winner);
@@ -27,24 +28,17 @@ public class Auctioner {
 
         for(Arbre a : escenari.Arbres){
             Voyage winner  = null;
-            List<Voyage> offers = new ArrayList<>();
+            TreeSet<Voyage> offers = new TreeSet<>();
             for(Robot r : escenari.Robots){
-
+                offers.add(r.getOffer(a));
             }
             winner.getRobot().update(winner);
         }
 
-        List<Voyage> offers = new ArrayList<>();
+        TreeSet<Voyage> offers = new TreeSet<>();
         for(Robot r : escenari.Robots){
-
+            offers.add(r.getOffer(escenari.Diposit));
         }
-
-        // Tots els robots tenen els viatges cap als focs assignats. Per tant el què faran serà ordenar-los tots per l'heurístic i miraran d'apagar el màxim de focs possibles començant per els més propers.
-
-
-
-        // TODO : Si queden drons i no queden focs els diem als drons que quedin que vagin a apagar els focs que quedin.
-
 
     }
 }
